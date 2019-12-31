e97fd8ba2ddf9657bd829f07bc9876b7c8bd9640
M042 <boukobza.nathan@gmail.com>
2019-12-31 18:15:18 +0100

    working snake

---
+++ GameObject.py
@@ -0,0 +1,78 @@
+import random
+from path_algorithm import DijkstraAlgorithm, AStar
+
+
+class Apple:
+    x = 0
+    y = 0
+
+    def __init__(self, width, height, game):
+        self.width = width
+        self.height = height
+        self.set_x_and_y(game)
+
+    def set_x_and_y(self, game):
+        while True:
+            self.x = random.randint(0, self.width-1)
+            self.y = random.randint(0, self.height-1)
+            if game[self.x][self.y] == 0:
+                game[self.x][self.y] = 2
+                print("x:%d y:%d" % (self.x, self.y))
+                break
+
+
+class Snake:
+    direction = 0
+    path = list()
+
+    def __init__(self, width, height):
+        self.body = [(int(width / 2), int(height / 2))]
+
+    def find_apple(self, apple):
+        x_apple = apple.x
+        y_apple = apple.y
+        x_head, y_head = self.body[-1]
+        if x_apple == x_head and y_apple == y_head:
+            return True
+        return False
+
+    def move(self, game, apple):
+        if self.direction == 3:
+            x, y = self.body[-1]
+            if y-1 < 0:
+                y = 29
+            else:
+                y = y-1
+        if self.direction == 2:
+            x, y = self.body[-1]
+            if y+1 > 29:
+                y = 0
+            else:
+                y = y+1
+        if self.direction == 1:
+            x, y = self.body[-1]
+            if x+1 > 29:
+                x = 0
+            else:
+                x = x + 1
+        if self.direction == 0:
+            x, y = self.body[-1]
+            if x-1 < 0:
+                x = 29
+            else:
+                x = x-1
+
+        game[x][y] = 1
+        self.body.append((x, y))
+
+        if self.find_apple(apple) is False:
+            tail_x, tail_y = self.body[0]
+            game[tail_x][tail_y] = 0
+            self.body.pop(0)
+        else:
+            apple.set_x_and_y(game)
+            path_algorithm = DijkstraAlgorithm(self, apple)
+            self.path = path_algorithm.test()
+        self.direction = self.path[-1]
+        if len(self.path) != 1:
+            self.path.pop(-1)
---
+++ main.py
@@ -0,0 +1,66 @@
+import pyglet
+from GameObject import Snake, Apple
+from path_algorithm import DijkstraAlgorithm, AStar
+
+
+class GameWindow(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.frame_rate = 1 / 60.0
+        n = 30
+        m = 30
+        self.height_cube = int(1200/n)
+        self.width_cube = int(900/m)
+        self.game = [0] * n
+        for i in range(n):
+            self.game[i] = [0] * m
+
+        self.snake = Snake(n, m)
+        self.apple = Apple(n, m, self.game)
+
+        path_algo = DijkstraAlgorithm(self.snake, self.apple)
+        self.snake.path = path_algo.test()
+
+        self.snake.direction = self.snake.path[-1]
+        self.snake.path.pop(-1)
+
+        self.game[self.apple.x][self.apple.y] = 2
+
+    def draw_polygon(self):
+        for x, y in self.snake.body:
+            pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                                 ('v2i', (x*self.height_cube, y*self.width_cube,
+                                          x*self.height_cube, y*self.width_cube + self.width_cube,
+                                          x*self.height_cube + self.height_cube, y*self.width_cube + self.width_cube,
+                                          x*self.height_cube + self.height_cube, y*self.width_cube)))
+        x = self.apple.x
+        y = self.apple.y
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                             ('v2i', (x * self.height_cube, y * self.width_cube,
+                                      x * self.height_cube, y * self.width_cube + self.width_cube,
+                                      x * self.height_cube + self.height_cube, y * self.width_cube + self.width_cube,
+                                      x * self.height_cube + self.height_cube, y * self.width_cube)),
+                             ('c3B', (255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0)))
+
+    def on_draw(self):
+        self.clear()
+        self.draw_polygon()
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == pyglet.window.key.LEFT:
+            self.snake.direction = 0
+        if symbol == pyglet.window.key.RIGHT:
+            self.snake.direction = 1
+        if symbol == pyglet.window.key.UP:
+            self.snake.direction = 2
+        if symbol == pyglet.window.key.DOWN:
+            self.snake.direction = 3
+
+    def update(self, dt):
+        self.snake.move(self.game, self.apple)
+
+
+if __name__ == "__main__":
+    window = GameWindow(1200, 900, "Snake", resizable=False)
+    pyglet.clock.schedule_interval(window.update, window.frame_rate)
+    pyglet.app.run()
